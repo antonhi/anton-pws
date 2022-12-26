2b5a563579516e9a67dbbd371fc2ff90a3bd0d6a
Anton <antonhpersonal@gmail.com>
2022-12-26 11:48:22 -0600

    Feat: Initial Fulfillment

--- lib/ordering/v1/fufillment.ts
+++ lib/ordering/v1/fufillment.ts
@@ -1,4 +1,10 @@
+import { InitialOrder } from "../../../models/orders";
+
 export function Fulfillment(consumerKey : string, consumerSecret: string, csn: string) {
+
+    function placeInitialOrder(order: InitialOrder) {
+        
+    }
 
     function printCSN() {
         console.log(csn);
